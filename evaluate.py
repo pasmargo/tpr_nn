eacdac5ef311651904993e016d6f2bbf73c59a7c
Pascual <pasmargo@gmail.com>
2018-04-10 13:55:11 +0800

    Added TODOs in source code

--- evaluate.py
+++ evaluate.py
@@ -5,6 +5,7 @@ import numpy as np
 seed = 23
 np.random.seed(seed=seed)
 
+from preprocessing import load_au_file
 from preprocessing import print_model_architecture
 
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
@@ -14,17 +15,22 @@ from keras.layers import Dense
 from keras.layers import Input
 from keras.models import Model
 
-num_feats = 10
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+num_feats = 3
 num_types_au = 6 # activity unit types.
 latent_dim = 10
 batch_size = 100
 patience = 6
 model_name = 'test'
-max_num_epochs = 20
+max_num_epochs = 200
 
 # Feats is a matrix N x F with N rows (activity units) and F columns (features of AUs).
 # Labels is a matrix N x C where C is the number of classes.
 Feats, Labels = load_au_file('P01_T1.au')
+print(Labels)
+print(Feats[:7,:])
+
 assert Labels.shape[1] == num_types_au, 'Unexpected number of AU types: {0} vs {1}'.format(
     Labels.shape[1], num_types_au)
 
@@ -46,19 +52,22 @@ model = Model(inputs=[au_feats], outputs=[x])
 # This is the compilation for a classification model.
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
 # This is the compilation for a regression model.
-model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
+# model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
 
 callbacks = [
     EarlyStopping(patience=patience),
     ReduceLROnPlateau(patience=patience / 2, verbose=1),
-    CSVLogger(filename=model_name + '.log.csv'),
-    ModelCheckpoint(model_name + '.check',
-                    save_best_only=True,
-                    save_weights_only=True)]
+    CSVLogger(filename=model_name + '.log.csv')]
     
+# ModelCheckpoint(model_name + '.check',
+#                 save_best_only=True,
+#                 save_weights_only=True)]
+# TODO: Split into training and validation in a meaningful way. (e.g. using a different file).
+# TODO: use features of previous AU to predict the current AU type.
+# TODO: Use LSTM.
 model.fit(
     Feats,
-    labels,
+    Labels,
     batch_size=batch_size,
     epochs=max_num_epochs,
     validation_split=0.2,
--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,45 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import logging
+import numpy as np
 
-# def load_au_file(fname):
-#     with open(fname, newline='') as csvfile:
-#         aureader = csv.DictReader(csvfile, delimiter='\t')
-#         for row in aureader:
-#             print(row['Time'])
+activity_mapping = {
+    1 : 0,
+    2 : 1,
+    4 : 2,
+    5 : 3,
+    6 : 4,
+    8 : 5}
+
+def get_id_offset(id_str):
+    ids = map(int, id_str.split('+'))
+    return max(ids) - min(ids)
+
+def get_id_max(id_str):
+    ids = map(int, id_str.split('+'))
+    return max(ids)
+
+def load_au_file(fname, num_classes=6, num_feats=3):
+    rows = []
+    with open(fname, newline='') as csvfile:
+        aureader = csv.DictReader(csvfile, delimiter='\t')
+        rows = [row for row in aureader]
+    logging.info('Read {0} data rows'.format(len(rows)))
+    Labels = np.zeros((len(rows), num_classes))
+    Features = np.zeros((len(rows), num_feats))
+    prev_maximum_src = 1
+    prev_maximum_trg = 1
+    for i, row in enumerate(rows):
+        activity_id = int(row['Type'])
+        Labels[i, activity_mapping[activity_id]] = 1.0
+        # Features: Dur STid    TTid
+        Features[i, 0] = float(row['Dur'])
+        Features[i, 1] = get_id_max(row['STid']) - prev_maximum_src
+        prev_maximum_src = get_id_max(row['STid'])
+        Features[i, 2] = get_id_max(row['TTid']) - prev_maximum_trg
+        prev_maximum_trg = get_id_max(row['TTid'])
+    return Features, Labels
 
 def print_model_architecture(model, fname):
     with open(fname, 'w') as fh:
